314e2ad1900e237eb832dd1fff4e78fc324ac16d
Fatima <fatima.moumni@hotmail.fr>
2022-05-11 13:49:09 +0200

    fix bugs

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/service/CurveServiceImpl.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/CurveServiceImpl.java
@@ -7,8 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 @Service
+@Transactional
 public class CurveServiceImpl implements CurveService {
     /**
      * SLF4J/LOG4J LOGGER instance.
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/service/TradeServiceImpl.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/TradeServiceImpl.java
@@ -8,7 +8,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 public class TradeServiceImpl implements TradeService{
 
     /**
