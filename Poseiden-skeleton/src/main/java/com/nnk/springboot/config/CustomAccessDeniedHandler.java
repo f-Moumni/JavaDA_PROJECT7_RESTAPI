c5a84c68acfce50c2f29f463884e9a8be1c98e32
Fatima <fatima.moumni@hotmail.fr>
2022-05-16 15:03:04 +0200

    add logs

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/config/CustomAccessDeniedHandler.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/CustomAccessDeniedHandler.java
@@ -1,5 +1,8 @@
 package com.nnk.springboot.config;
 
+import com.nnk.springboot.service.BidServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
@@ -9,9 +12,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    /**
+     * SLF4J/LOG4J LOGGER instance.
+     */
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(CustomAccessDeniedHandler.class);
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException arg2)
             throws IOException, ServletException {
+        LOGGER.error("unauthorized access ");
         response.sendRedirect(request.getContextPath() + "/403");
     }
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/config/CustomOAuth2User.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/CustomOAuth2User.java
@@ -3,31 +3,62 @@ package com.nnk.springboot.config;
 import java.util.Collection;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.GrantedAuthority;
 
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 public class CustomOAuth2User implements OAuth2User {
 
+    /**
+     * SLF4J/LOG4J LOGGER instance.
+     */
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(CustomOAuth2User.class);
+
+    /** OAuth2user**/
+
     private OAuth2User oauth2User;
 
+
+    /**
+     * CustomOAuth2User constructor
+     * @param oauth2User
+     */
     public CustomOAuth2User(OAuth2User oauth2User) {
+
         this.oauth2User = oauth2User;
     }
 
 
+    /**
+     * * {@inheritDoc}
+     */
+
     @Override
     public Map<String, Object> getAttributes() {
+        LOGGER.debug("getting 0Auth2User attributes ");
         return oauth2User.getAttributes();
     }
 
+    /**
+     * * {@inheritDoc}
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        LOGGER.debug("getting 0Auth2User authorities");
         return oauth2User.getAuthorities();
     }
 
+    /**
+     * Get the OAuth 2.0 token attribute login
+     * Returns:
+     * the attribute login or null otherwise
+     */
     @Override
     public String getName() {
+        LOGGER.debug("getting 0Auth2User login");
         return oauth2User.getAttribute("login");
     }
 
